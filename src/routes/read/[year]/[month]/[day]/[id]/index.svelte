c526d5e312ad5d8233c182fa7263b8e8e68be571
Rizal Muhamad H <rizal.muhamad@ayomedia.com>
2020-01-23 12:13:57 +0700

    update & delate

---
+++ src/routes/read/[year]/[month]/[day]/[id]/index.svelte
@@ -0,0 +1,12 @@
+<script context="module">
+  export function preload(page) {
+    return { year: page.params.year, id: page.params.id };
+  }
+</script>
+
+<script>
+  export let year;
+  export let id;
+</script>
+
+<div>{year} {id}</div>
